bfeeed7207bddbe17b8139f507d4d6f8a6a4449b
farruwasiq <farruwasiq@gmail.com>
2023-02-09 20:58:57 -0800

    install apache

--- main.tf
+++ main.tf
@@ -1,10 +1,110 @@
 provider "aws" {
 
-  
 }
-resource "aws_instance" "web" {
-    count = 2
-    instance_type = "t2.micro"
-    ami = "ami-0aa7d40eeae50c9a9"
-    
-}  
+
+
+
+resource "aws_instance" "my-instance" {
+  ami           = "ami-0aa7d40eeae50c9a9"
+  instance_type = "t2.nano"
+  key_name      = "aws-ec2"
+  user_data     = file("install_apache.sh")
+  tags = {
+    Name = "Terraform"
+
+  }
+}
+/*
+# Creating VPC
+resource "aws_vpc" "demovpc" {
+  cidr_block           = var.vpc_cidr
+  instance_tenancy     = "default"
+  enable_dns_hostnames = true
+  tags = {
+    Name = "Demo VPC"
+  }
+}
+# Creating 1st web subnet 
+resource "aws_subnet" "public-subnet-1" {
+  vpc_id                  = aws_vpc.demovpc.id
+  cidr_block              = var.subnet_cidr
+  map_public_ip_on_launch = true
+  availability_zone       = "us-east-1a"
+  tags = {
+    Name = "Web Subnet 1"
+  }
+}
+# Creating 2nd web subnet 
+resource "aws_subnet" "public-subnet-2" {
+  vpc_id                  = aws_vpc.demovpc.id
+  cidr_block              = var.subnet1_cidr
+  map_public_ip_on_launch = true
+  availability_zone       = "us-east-1b"
+  tags = {
+    Name = "Web Subnet 2"
+  }
+}
+# Creating 1st application subnet 
+resource "aws_subnet" "application-subnet-1" {
+  vpc_id                  = aws_vpc.demovpc.id
+  cidr_block              = var.subnet2_cidr
+  map_public_ip_on_launch = false
+  availability_zone       = "us-east-1a"
+  tags = {
+    Name = "Application Subnet 1"
+  }
+}
+# Creating 2nd application subnet 
+resource "aws_subnet" "application-subnet-2" {
+  vpc_id                  = aws_vpc.demovpc.id
+  cidr_block              = var.subnet3_cidr
+  map_public_ip_on_launch = false
+  availability_zone       = "us-east-1b"
+  tags = {
+    Name = "Application Subnet 2"
+  }
+}
+# Create Database Private Subnet
+resource "aws_subnet" "database-subnet-1" {
+  vpc_id            = aws_vpc.demovpc.id
+  cidr_block        = var.subnet4_cidr
+  availability_zone = "us-east-1a"
+  tags = {
+    Name = "Database Subnet 1"
+  }
+}
+# Create Database Private Subnet
+resource "aws_subnet" "database-subnet-2" {
+  vpc_id            = aws_vpc.demovpc.id
+  cidr_block        = var.subnet5_cidr
+  availability_zone = "us-east-1a"
+  tags = {
+    Name = "Database Subnet 1"
+  }
+}
+# Creating Internet Gateway 
+resource "aws_internet_gateway" "demogateway" {
+  vpc_id = aws_vpc.demovpc.id
+}
+# Creating Route Table
+resource "aws_route_table" "route" {
+  vpc_id = aws_vpc.demovpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.demogateway.id
+  }
+  tags = {
+    Name = "Route to internet"
+  }
+}
+# Associating Route Table
+resource "aws_route_table_association" "rt1" {
+  subnet_id      = aws_subnet.demosubnet.id
+  route_table_id = aws_route_table.route.id
+}
+# Associating Route Table
+resource "aws_route_table_association" "rt2" {
+  subnet_id      = aws_subnet.demosubnet1.id
+  route_table_id = aws_route_table.route.id
+}
+*/
